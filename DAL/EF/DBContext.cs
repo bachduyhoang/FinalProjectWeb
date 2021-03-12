5b54f1ccb7d83364f5bbb1d01e447cda13a2038e
BachDuyHoang <bachduyhoang2k@gmail.com>
2021-03-12 16:13:21 +0700

    Done

--- DAL/EF/DBContext.cs
+++ DAL/EF/DBContext.cs
@@ -19,7 +19,6 @@ namespace DAL.EF
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
