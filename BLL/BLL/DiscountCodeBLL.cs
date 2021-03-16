a1c44b04abedceeda21f0bd60780856caaa19228
truong huy <truongnhhse140562@fpt.edu.vn>
2021-03-17 06:59:53 +0700

    crud Discount

--- BLL/BLL/DiscountCodeBLL.cs
+++ BLL/BLL/DiscountCodeBLL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,72 @@ using System.Threading.Tasks;
 
 namespace BLL.BLL
 {
-    class DiscountCodeBLL
+    public class DiscountCodeBLL
     {
+        DBContext context = null;
+        public DiscountCodeBLL()
+        {
+            context = new DBContext();
+        }
+
+        public List<Discount> GetList(int currentPage, string searchWord)
+        {
+            int PAGE_SIZE = 1;
+            var list = context.Discounts.OrderBy(x => x.discountID).Skip((currentPage - 1) * PAGE_SIZE).Take(PAGE_SIZE).Where(x => x.discountID.Contains(searchWord)).ToList();
+            return list;
+        }
+
+        public int CountPage(string searchWord)
+        {
+            int PAGE_SIZE = 1;
+            int totalPage = 0;
+            int totalRow = context.Discounts.Where(x => x.discountID.Contains(searchWord)).Count();
+            if (totalRow % PAGE_SIZE > 0)
+            {
+                totalPage = totalRow / PAGE_SIZE + 1;
+            }
+            else
+            {
+                totalPage = totalRow / PAGE_SIZE;
+            }
+            return totalPage;
+        }
+
+        public int CheckIdExisted(string DiscountID)
+        {
+            var result = context.Discounts.Where(x => x.discountID == DiscountID).Count();
+            return result;
+        }
+
+        public Discount Find(string DiscountID)
+        {
+            Discount dis = context.Discounts.Find(DiscountID);
+            return dis;
+        }
+
+        public void Insert(Discount discount)
+        {
+            discount.dayCreated = DateTime.Now;
+            context.Discounts.Add(discount);
+            context.SaveChanges();
+        }
+
+        public void Delete(string discountID)
+        {
+            var dis = new Discount { discountID = discountID};
+            context.Entry(dis).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public void Update(Discount dis)
+        {
+            var result = context.Discounts.SingleOrDefault(x => x.discountID == dis.discountID);
+            if(result != null)
+            {
+                result.discountPercent = dis.discountPercent;
+                result.expiry = dis.expiry;
+                context.SaveChanges();
+            }
+        }
     }
 }
---
+++ FinalProjectWeb/Areas/Admin/Controllers/DiscountController.cs
@@ -0,0 +1,106 @@
+﻿using BLL.BLL;
+using DAL.EF;
+using FinalProjectWeb.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProjectWeb.Areas.Admin.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // GET: Admin/Discount
+        public ActionResult Index()
+        {
+            var dll = new DiscountCodeBLL();
+            var model = new DiscountModel();
+            model.currentPage = 1;
+            model.totalPage = dll.CountPage("");
+            model.list = dll.GetList(1, "");
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(DiscountModel model)
+        {
+            if(model.searchWord == null)
+            {
+                model.searchWord = "";
+            }
+            if(model.currentPage == 0)
+            {
+                model.currentPage = 1;
+            }
+            var dll = new DiscountCodeBLL();
+            model.totalPage = dll.CountPage(model.searchWord);
+            model.list = dll.GetList(model.currentPage, model.searchWord);
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Discount discount)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    DiscountCodeBLL bll = new DiscountCodeBLL();
+                    int result = bll.CheckIdExisted(discount.discountID);
+                    if(result == 0)
+                    {
+                        bll.Insert(discount);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "ID EXISTED");
+                        
+                    }
+                    return View(discount);
+                }
+                return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(DiscountModel model)
+        {
+            DiscountCodeBLL bll = new DiscountCodeBLL();
+            bll.Delete(model.discountID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string DiscountID)
+        {
+            DiscountCodeBLL bll = new DiscountCodeBLL();
+            var dis = bll.Find(DiscountID);
+            return View(dis);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Discount dis)
+        {
+            if (ModelState.IsValid)
+            {
+                DiscountCodeBLL bll = new DiscountCodeBLL();
+                bll.Update(dis);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
