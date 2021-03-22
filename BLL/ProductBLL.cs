cb66536b26bfaedc561449782d6b950a86e3d126
BachDuyHoang <bachduyhoang2k@gmail.com>
2021-03-22 09:15:12 +0700

    reput ProductBLL

--- BLL/ProductBLL.cs
+++ BLL/ProductBLL.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DAL.DAL;
-using DAL.EF;
 
 namespace BLL
 {
@@ -17,16 +15,46 @@ namespace BLL
 
         public ProductBLL()
         {
+            p = new ProductDAL();
         }
-        public List<Product>GetListNew()
+        public List<Product> GetListNew()
         {
             return p.GetListNew();
         }
 
         public List<Brand> GetListBrand()
         {
+            return p.GetListBrand();
         }
 
+        public List<Product> GetListOfBrand(string brandName, ref int totalRecord, int index = 1, int maxPage = 20)
+        {
+            return p.GetListOfBrand(brandName, ref totalRecord, index, maxPage);
+        }
 
+        public Product GetProduct(int id)
+        {
+            return p.GetProduct(id);
+        }
+
+        public List<Product> GetListAllProduct(string name, ref int totalRecord, int index = 1, int maxPage = 20)
+        {
+            return p.GetListAllProduct(name, ref totalRecord, index, maxPage);
+        }
+
+        public int GetMaxQuantity(int id)
+        {
+            return p.GetMaxQuantity(id);
+        }
+
+        public int InsertOrder(Order o)
+        {
+            return p.InsertOrder(o);
+        }
+
+        public void InsertOrderDetail(OrderDetail detail)
+        {
+            p.InsertOrderDetail(detail);
+        }
     }
 }
